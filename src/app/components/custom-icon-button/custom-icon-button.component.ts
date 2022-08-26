2acd037c9dc2af28fc1f9abd49e48467257b1f80
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-08-26 16:05:25 +0300

    [feat] styling custom icon button

--- src/app/components/custom-icon-button/custom-icon-button.component.ts
+++ src/app/components/custom-icon-button/custom-icon-button.component.ts
@@ -1,24 +1,29 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-custom-icon-button',
-  templateUrl: './custom-icon-button.component.html',
-  styleUrls: ['./custom-icon-button.component.scss'],
+	selector: 'app-custom-icon-button',
+	templateUrl: './custom-icon-button.component.html',
+	styleUrls: ['./custom-icon-button.component.scss'],
 })
-export class CustomIconButtonComponent{
+export class CustomIconButtonComponent {
 
 	@Input()
 	id: number;
 	@Input()
 	name: string;
+	@Input()
+	height = 62;
+	@Input()
+	size: string;
 
 	@Output()
 	clickBtn: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+	constructor() {
+	}
 
 	onClickBtn(id) {
-	  this.clickBtn.emit(id);
+		this.clickBtn.emit(id);
 	}
 
 }
