0126b2d86eeea047ac96a8503fea7b1f83af3311
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-08-31 15:45:50 +0300

    [feat] new icons

--- src/app/models/icons.model.ts
+++ src/app/models/icons.model.ts
@@ -1,9 +1,14 @@
 export enum Icons {
 	arrowLeft = 'arrow-left',
+	arrowRight = 'arrow-right',
 	arrowUp = 'arrow-up',
 	arrowUpFill = 'arrow-up-fill',
 	arrowDown = 'arrow-down',
 	arrowDownFill = 'arrow-down-fill',
 	filterCrisis = 'filter-crisis',
 	menu = 'menu',
+	month = 'month',
+	quaterYear = 'quater-year',
+	halfYear = 'half-year',
+	year = 'year'
 }
