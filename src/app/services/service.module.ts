3d78c4f7ea471f94b5643d0f9bb9baa29c526f53
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-08-31 16:07:57 +0300

    [fix] remove modal service

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -5,12 +5,11 @@ import {HttpClientModule} from '@angular/common/http';
 import {CrisisService} from './crisis.service';
 import {TableService} from './table.service';
 import {LogoService} from './logo.service';
-import {ModalService} from './modal.service';
 import {DomService} from './dom.service';
 
 @NgModule({
   imports: [HttpClientModule],
-  providers: [WebApiService, AppService, CrisisService, TableService, LogoService, ModalService, DomService],
+  providers: [WebApiService, AppService, CrisisService, TableService, LogoService, DomService],
   // exports: [WebApiService, AppService]
 })
 export class ServiceModule {}
