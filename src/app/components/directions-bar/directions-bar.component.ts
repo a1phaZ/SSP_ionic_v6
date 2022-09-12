7effb9b38a06587e2c61d9a31840aa10e2a6d0d3
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-09-12 15:38:56 +0300

    [feat] direction bar component with store

--- src/app/components/directions-bar/directions-bar.component.ts
+++ src/app/components/directions-bar/directions-bar.component.ts
@@ -1,22 +1,45 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TDirection} from '../../models/direction.model';
-import {ItemsBarComponent} from '../base/items-bar/items-bar.component';
+import {ActivatedRoute} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {filterDirectionsList, setCurrentDirection} from '../../../store/directions/directions.actions';
+import {switchMap} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {selectCurrentDirection, selectDirections} from '../../../store/directions/directions.selectors';
+import {IAppState} from '../../../store/app.state';
 
 @Component({
-  selector: 'app-directions-bar',
-  templateUrl: './directions-bar.component.html',
-  styleUrls: ['../base/items-bar/items-bar.component.scss','./directions-bar.component.scss'],
+	selector: 'app-directions-bar',
+	templateUrl: './directions-bar.component.html',
+	styleUrls: ['../base/items-bar/items-bar.component.scss', './directions-bar.component.scss'],
 })
-export class DirectionsBarComponent extends ItemsBarComponent implements OnInit {
+export class DirectionsBarComponent implements OnInit {
 
-	@Input()
-	items: TDirection[];
+	directionsList$: Observable<TDirection[]>;
+	currentDirection$: Observable<number>;
 
-  constructor() {
-	  super();
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private store: Store<IAppState>,
+	) {
+		this.route.params.pipe(
+			switchMap((data: { buttonId: number }) => {
+				this.store.dispatch(filterDirectionsList({buttonId: Number(data.buttonId)}));
+				return of(true);
+			})
+		).subscribe(() => {
+			this.directionsList$ = this.store.select(selectDirections);
+			this.currentDirection$ = this.store.select(selectCurrentDirection);
+		});
+	}
 
-  ngOnInit() {}
+	ngOnInit() {
+
+	}
+
+	changeSelected(id: number) {
+		this.store.dispatch(setCurrentDirection({directionId: id}));
+	}
 
 
 }
