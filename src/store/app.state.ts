6ea1b9e3e8300f57cf24576dc27796e550a296e6
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-09-15 12:03:58 +0300

    [feat] organization store

--- src/store/app.state.ts
+++ src/store/app.state.ts
@@ -1,9 +1,11 @@
 import {IDashboardState} from './dashboard/dashboard.reducer';
 import {IDirectionState} from './directions/directions.reducer';
 import {IPeriodState} from './period-picker/period-picker.reducer';
+import {IOrganizationsState} from './organizations/organizations.reducer';
 
 export interface IAppState {
 	dashboard: IDashboardState;
 	directions: IDirectionState;
 	periods: IPeriodState[];
+	organizations: IOrganizationsState;
 }
