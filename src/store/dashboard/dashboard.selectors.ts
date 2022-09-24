9ecb698cddc07c91d597b9b9ce5a92c0aba9ace1
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-09-24 21:22:20 +0300

    [feat] select available types

--- src/store/dashboard/dashboard.selectors.ts
+++ src/store/dashboard/dashboard.selectors.ts
@@ -1,6 +1,7 @@
 import {IDashboardState} from './dashboard.reducer';
 import {IAppState} from '../app.state';
 import {createSelector} from '@ngrx/store';
+import {selectAppDashboard} from '../app.selectors';
 
 export const getState = (state: IAppState) => state.dashboard;
 
@@ -12,4 +13,9 @@ export const selectDashboardSelected = createSelector(
 export const selectDashboardPrevSelected = createSelector(
 	getState,
 	(state: IDashboardState) => state.prevSelected
+);
+
+export const selectAvailableTypes = createSelector(
+	selectAppDashboard,
+	(state: IDashboardState) => state.selected.availableTypes
 );
